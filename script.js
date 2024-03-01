079756e8ea1bad5b727f426f19bd4dd237e59314
frostbitepy <fran94rv@gmail.com>
2024-03-01 15:30:38 -0300

    Handled API call error

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let intentos = 6;
-//let listado_palabras = ['LIMON', 'TIGRE', 'SALIR', 'ROBLE', 'GRUPO', 'ZORRO', 'CARNE', 'CALOR', 'PERRO', 'PLUMA'];
+let listado_palabras = ['LIMON', 'TIGRE', 'SALIR', 'ROBLE', 'GRUPO', 'LUNAR', 'CARNE', 'CALOR', 'POLAR', 'PLUMA'];
 // let palabra = listado_palabras[Math.floor(Math.random() * listado_palabras.length)];
 
 
@@ -8,59 +8,61 @@ const INPUT = document.getElementById("guess-input");
 const VALOR = INPUT.value;
 const CONTENEDOR = document.getElementById("guesses");
 const GRID = document.getElementById("grid");
-const openaiApiKey = 'test'; // Replace with your OpenAI API key
+const openaiApiKey = 'some_api_key'; // Replace with your OpenAI API key
 
 
 window.addEventListener('load', init);
 
-// Supongamos que tienes una variable secreta llamada MY_SECRET_KEY
-const secretKey = process.env.API_KEY;
+setup();
 
 
 
+async function intentar(WORD) {
+	const USER_INPUT = read_input();
+	const ROW = document.createElement("div");
+	ROW.className = "row";
+	console.log("Al darle intentar la palabra es: " + WORD);
+	const palabra = WORD.toUpperCase();
+	console.log("Don't cheat!! " + palabra);
+
+	if (USER_INPUT === palabra){
+		winning(ROW, USER_INPUT, palabra);
+		return;
+	} else {
+		continuing(ROW, USER_INPUT, palabra);
+	}		
+
+	intentos --; 
+	
+	losing(intentos, palabra);
+}
+
+function init() {
+	console.log("El trigger funciona!");
+}
 
 async function setup() {
-    const WORD = await api_call();
-
+    WORD = await api_call();
     BUTTON.addEventListener('click', function () {
         intentar(WORD);
     });
 }
 
-setup();
-
-async function api_call() {
-	const response = await fetch('https://api.openai.com/v1/chat/completions', {
-    	method: 'POST',
-    	headers: {
-      	'Content-Type': 'application/json',
-      	'Authorization': `Bearer ${openaiApiKey}`,
-    	},
-    	body: JSON.stringify({
-      		model: 'gpt-3.5-turbo',
-			messages: [{"role": "user", "content": "Genera una palabra aleatoria en español de 5 letras, devuelve solo la una palabra donde palabra.len == 5 "}],
-      		// prompt: 'Genera una palabra en español de 5 letras:',
-      		max_tokens: 5,
-      		temperature: 0.6, // Set temperature to the maximum for more randomness
-    	}),
-    });
-
-	const result = await response.json();
-	console.log(result);
-	console.log(result.choices[0]?.message?.content);
-  	const generatedWord = result.choices[0]?.message?.content;
-
-	return generatedWord.trim();
+function read_input() {
+	let intento = INPUT; 
+	intento = intento.value;
+	intento = intento.toUpperCase();
+	return intento;
 }
 
-async function intentar(WORD) {
-	const USER_INPUT = leerIntento();
-	const ROW = document.createElement("div");
-	ROW.className = "row";
-	console.log("Al darle intentar la palabra es: " + WORD);
-	const palabra = WORD.toUpperCase();
+function terminar(mensaje) {
+	INPUT.style.display = "none";
+	BUTTON.style.display = "none";
+	CONTENEDOR.style.display = "block";
+	CONTENEDOR.innerHTML = mensaje;
+}
 
-	if (USER_INPUT === palabra) {
+function winning(ROW, USER_INPUT, palabra){
 		console.log("Al ganar la palabra es: " + palabra)
 		terminar("GANASTE!");
 		for (let i in USER_INPUT) {
@@ -72,8 +74,9 @@ async function intentar(WORD) {
 		}
 		GRID.appendChild(ROW);
 		return;
-	}
-	
+}
+
+function continuing(ROW, USER_INPUT, palabra){
 	for (let i in palabra) {
 		console.log("dentro del for palabra: " + palabra)
 		const SPAN = document.createElement("span");
@@ -94,31 +97,38 @@ async function intentar(WORD) {
 		ROW.appendChild(SPAN);
 	}
 	GRID.appendChild(ROW);	
+}
 
-	intentos --; // Reduce la cantidad de intentos
-	
+function losing(intentos, palabra) {
 	if (intentos == 0 ) {
-		console.log("al perder la palabra es: " + palabra);
+		console.log("Se acabaron los intentos, la palabra era: " + palabra);
 		terminar("PERDISTE!");
 	}
 }
 
-function init() {
-	console.log("El trigger funciona!");
-	console.log(secretKey);
-}
+async function api_call() {
+	const response = await fetch('https://api.openai.com/v1/chat/completions', {
+    	method: 'POST',
+    	headers: {
+      	'Content-Type': 'application/json',
+      	'Authorization': `Bearer ${openaiApiKey}`,
+    	},
+    	body: JSON.stringify({
+      		model: 'gpt-3.5-turbo',
+			messages: [{"role": "user", "content": "Genera una palabra aleatoria en español de 5 letras, devuelve solo la una palabra donde palabra.len == 5 "}],
+      		temperature: 0.6, 
+    	}),
+    });
 
-function leerIntento() {
-	let intento = INPUT; 
-	intento = intento.value;
-	intento = intento.toUpperCase();
-	return intento;
-}
+	const result = await response.json();
 
-function terminar(mensaje) {
-	INPUT.style.display = "none";
-	BUTTON.style.display = "none";
-	CONTENEDOR.style.display = "block";
-	CONTENEDOR.innerHTML = mensaje;
+	if (!response.ok) {
+        const generateWord = listado_palabras[Math.floor(Math.random() * listado_palabras.length)];
+		return generateWord
+    } else {
+		console.log(result);
+		console.log(result.choices[0]?.message?.content);
+  		const generatedWord = result.choices[0]?.message?.content;
+		return generatedWord.trim();
+	}	
 }
-
